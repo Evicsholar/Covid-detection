cb592bc050b3f16182233d1ae38bb665f462c8e8
anonymous-dipsite <63680382+anonymous-dipsite@users.noreply.github.com>
2020-09-17 23:37:59 +0530

    Final

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-import os
+from os.path import join, dirname, realpath
 
-UPLOAD_FOLDER = 'Uploads'
+UPLOADS_PATH = join(dirname(realpath(__file__)), 'static\\imgages')
+
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 
@@ -11,9 +13,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/inputform", methods=["GET", "POST"])
 def input_symptoms():
@@ -22,31 +26,14 @@ def input_symptoms():
         tiredcough = str(request.form['commonsym'])
         breath = str(request.form['majorsym'])
         exposure = str(request.form['exposure'])
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-        if 0 == 0:
-            result = 'here, '+travel, tiredcough, breath, exposure+" you go"
-            return render_template('index.html', results=result)
+        try:
+
+            image = request.files['image']
+
+            image.save(os.path.join(UPLOADS_PATH, secure_filename(image.filename)))
+        except:
+            return render_template('index.html', results="No File Found")
+
+        result = 'here, ' + travel, tiredcough, breath, exposure + " you go"
+        return render_template('index.html', results=result)
     return None
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
